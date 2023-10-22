732cd98e4d6ef75a42a716f2bd5c8f3c8667d9e6
Youssef <youssefalsoufi.1@gmail.com>
2023-10-22 23:35:42 +0200

    Use Cors to add policy to my API.

--- Program.cs
+++ Program.cs
@@ -3,27 +3,44 @@ using Microsoft.Extensions.DependencyInjection;
 using ConverterRestApi.Data;
 using System.Drawing.Text;
 using ConverterRestApi;
-
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ConverterRestApiContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConverterRestApiContext") ?? throw new InvalidOperationException("Connection string 'ConverterRestApiContext' not found.")));
 
 
-var dbContext = builder.Services.BuildServiceProvider().GetService<ConverterRestApiContext>();
+var dbContext = builder.Services.BuildServiceProvider().GetService<ConveterTools>();
+
+//var origins = new List<string>();
+//if (dbContext != null)
+//{
+//    var originData = dbContext.Origins.Where(item => item.IsActive == true).ToList();
+//    if (originData.Any() && originData != null)
+//    {
+//        originData.ForEach(item =>
+//        origins.Add(item.OriginName));
+//    }
+//}
+
+
 builder.Services.AddScoped<DataAccess>();
 builder.Services.AddScoped<ConveterTools>();
-//var te = dbContext?.DataUnits;
-//DataAccess n = new(dbContext);
-
-//ConveterTools tool = new( n);
-//tool.DoConvert("1","kilometer", "meter");
 
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(p => p.AddPolicy("CorsPolicy", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();  // here if we fill WithOrigins("http://localhost:4200") means only this domain ables to access my API.
+                                                               // Or if we fill WithOrigins("http://localhost:4200","http://localhost:3000") for multi domains.
+                                                               // in case more than 5 domains , you have to choose : any domain means : build.WithOrigins("*")...
+
+    // instead of using "*", we can read a set of origins from db as we did top with origins list. so we can put origins.ToArray() instead of "*".
+}));
 
 var app = builder.Build();
 
@@ -39,5 +56,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors("CorsPolicy");
 
 app.Run();
