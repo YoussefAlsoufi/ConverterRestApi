359507ae99e1a99d2ffae4be99124933d87b6646
Youssef <youssefalsoufi.1@gmail.com>
2023-12-15 00:13:45 +0100

    update checking Password validity

--- Helper/CheckInputsValidity.cs
+++ Helper/CheckInputsValidity.cs
@@ -22,6 +22,46 @@ namespace ConverterRestApi.Helper
             string pattern = @"^\d{3}\d{3}\d{4}$";
             return Regex.IsMatch(phoneNumber,pattern);
         }
+        public static bool IsValidPassword(string phoneNumber)
+        {
+            string pattern = @"^\d{3}\d{3}\d{4}$";
+            return Regex.IsMatch(phoneNumber, pattern);
+        }
+        public static bool IsPasswordValid(string password)
+        {
+            // Check if the password is null or empty
+            if (string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+
+            // Check for at least one uppercase letter
+            if (!password.Any(char.IsUpper))
+            {
+                return false;
+            }
+
+            // Check for at least one lowercase letter
+            if (!password.Any(char.IsLower))
+            {
+                return false;
+            }
+
+            // Check for at least one digit
+            if (!password.Any(char.IsDigit))
+            {
+                return false;
+            }
+
+            // Check for at least one special character (non-alphanumeric)
+            if (password.All(char.IsLetterOrDigit))
+            {
+                return false;
+            }
+
+            // If all criteria are met, the password is valid
+            return true;
+        }
 
     } 
 }
